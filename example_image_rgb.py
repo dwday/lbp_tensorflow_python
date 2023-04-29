dde94707846d503d5e728bcc9742265b25d56c96
Devrim Akgun <47365878+dwday@users.noreply.github.com>
2023-04-29 14:35:39 +0300

    Update example_image_rgb.py

--- example_image_rgb.py
+++ example_image_rgb.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Test file for rgb and grascale outputs  
+Please cite below works if you find it useful:
+Akgun, D., A TensorFlow implementation of Local Binary Patterns Transform. MANAS Journal of Engineering, 9(1), 15-21. DOI:10.51354/mjen.822630
+Akgun, Devrim. "A PyTorch Operations Based Approach for Computing Local Binary Patterns." U. Porto Journal of Engineering 7.4 (2021): 61-69. https://doi.org/10.24840/2183-6493_007.004_0005
+"""
 """
 from lib.lbplib import py_lbp,tf_lbp
 from tensorflow.keras.preprocessing import image
